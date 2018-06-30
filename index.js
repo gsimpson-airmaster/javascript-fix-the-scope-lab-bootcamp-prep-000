39aa3cb9cfac4ace2cfc74ab5fd4669417455d4c
gsimpson@airmaster.com.au <gsimpson@airmaster.com.au>
2018-06-30 10:01:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function funkyFunction() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
-
+theFunk = theFunk.function()
 console.log(`the Funk = ${theFunk}`)
 console.log("Calling the function " + funkyFunction())
