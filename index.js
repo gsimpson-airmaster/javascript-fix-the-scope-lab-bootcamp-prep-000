7e3121c117bf7cba37b91ee955da1ec1bee0eaf0
gsimpson@airmaster.com.au <gsimpson@airmaster.com.au>
2018-06-30 10:09:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ var funkyFunction = function() {
   return funky();
 }
 
+function funkyFunction() {
+  return function() {}
+}
+
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
